ba6d58344863c0e9a1d3b2959b3b61c99583abc2
Edward Wang <edawordwang@gmail.com>
2025-01-02 00:40:22 -0500

    optimize struct packing

--- include/button/button.h
+++ include/button/button.h
@@ -1,16 +1,19 @@
-#include <functional>
-// #include <cstdint>
-class button {
+#include <cstdint>
+struct Button {
+    long start_time;
+    long end_time;
+    long time_between;
+
+    void (*update_function)(bool);
+
     int midi_control; // midi control number (0-127);
     int mux_position; // which pin of the mux to read (0-15)
     int mux_address;  // which mux to search in (0-2)
     int led_address;
 
-    bool state;
-    bool last_state;
     uint16_t start_state = 0;
     uint16_t end_state = 0;
-    long start_time;
-    long end_time;
-    long time_between;
+
+    bool state;
+    bool last_state;
 };
