400df083ea0e80c258c9fee35ee97b5f5d9e2214
Edward Wang <edawordwang@gmail.com>
2025-01-02 01:46:34 -0500

    move files and cleanup

--- src/button.h
+++ src/button.h
@@ -5,7 +5,14 @@ struct Button {
     uint8_t midi_control; // midi control number (0-127);
     uint8_t mux_address;  // which mux to search in (0-2)
     uint8_t mux_position; // which pin of the mux to read (0-15)
-    uint8_t led;
+    uint8_t led_address;
 
     bool state;
 };
+
+Button button_create(
+    int midi_control, int mux_address, int mux_position,
+    int led_address, void (*update_function)(bool));
+void button_set_state(Button *b, bool value);
+void button_update_led(Button *b);
+void button_read(Button *b);
