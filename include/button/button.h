9117cc1072cf0b044f01c8b554cd57eaa38977e9
Edward Wang <edawordwang@gmail.com>
2025-01-02 01:14:37 -0500

    remove unnecessary fields

--- include/button/button.h
+++ include/button/button.h
@@ -1,19 +1,11 @@
-#include <cstdint>
+#include <stdint.h>
 struct Button {
-    long start_time;
-    long end_time;
-    long time_between;
-
     void (*update_function)(bool);
 
-    int midi_control; // midi control number (0-127);
-    int mux_address;  // which mux to search in (0-2)
-    int mux_position; // which pin of the mux to read (0-15)
-    int led_address;
-
-    uint16_t start_state = 0;
-    uint16_t end_state = 0;
+    uint8_t midi_control; // midi control number (0-127);
+    uint8_t mux_address;  // which mux to search in (0-2)
+    uint8_t mux_position; // which pin of the mux to read (0-15)
+    uint8_t led;
 
     bool state;
-    bool last_state;
 };
